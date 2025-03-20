9dad95b2666f29c51303702e4fc7f23743359082
navpreetkaur4 <navu5393@icloud.com>
2025-03-20 18:19:45 -0500

    feat: integrate Helmet.js for security headers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import helmet from "helmet"; 
 import morgan from "morgan";
 import dotenv from "dotenv";
 import { setupSwagger } from "./swagger";
@@ -13,6 +14,7 @@ const app: Application = express();
 
 // Middleware
 app.use(express.json()); 
+app.use(helmet()); 
 app.use(cors()); 
 app.use(morgan("combined")); 
 
